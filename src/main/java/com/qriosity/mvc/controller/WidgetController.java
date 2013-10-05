ba469afe0272ff95abfc74b8ba6d16981b3102f8
yoandy <yterradas@gmail.com>
2013-10-05 13:24:02 -0700

    Changing the config and controller

--- src/main/java/com/qriosity/mvc/controller/WidgetController.java
+++ src/main/java/com/qriosity/mvc/controller/WidgetController.java
@@ -1,5 +1,10 @@
 package com.qriosity.mvc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +14,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author yoandy
  * @since 10/5/13
  */
-@Controller("/widget")
+@Controller
+@RequestMapping("/wigdet")
+@Scope
 public class WidgetController {
+    @Resource
+    VendorWidgetService vendorWidgetService;
 
     @RequestMapping(value = "/info/vendorId/{vid}/itemId/{iid}", method = RequestMethod.GET)
-    public String VendorInfo(@PathVariable("vid") final String vendorId, @PathVariable("iid") final String itemId) {
+    public Map<String, Object> VendorInfo(@PathVariable("vid") final String vendorId, @PathVariable("iid") final String itemId) {
+        Map<String, Object> rval = new HashMap<String, Object>();
 
-
-        return "";
+        return rval;
     }
 }
--- src/main/resources/AppConfig.java
+++ src/main/resources/AppConfig.java
@@ -1,6 +1,9 @@
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author yoandy
@@ -9,6 +12,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.qriosity.mvc.controller"})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/styles/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
