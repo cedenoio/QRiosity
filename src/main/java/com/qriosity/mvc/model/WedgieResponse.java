6dc702080817d1f847cdeaf42cfffd10e3156977
Aye <aye@fitiful.com>
2013-10-06 13:41:23 -0700

    Added wedgie creation code

---
+++ src/main/java/com/qriosity/mvc/model/WedgieResponse.java
@@ -0,0 +1,29 @@
+package com.qriosity.mvc.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ayethu
+ * Date: 10/6/13
+ * Time: 1:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WedgieResponse {
+    private String text;
+    private String id;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
