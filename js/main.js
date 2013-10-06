e6177b416deb06a9cec1130d32e51316cb804836
Steven McDaniel <smcdaniel@zappos.com>
2013-10-06 11:28:21 -0700

    code update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-$('.result img').click(function() {
-	$('.result img').removeClass('animate');
-	$(this).addClass('animate');
-});
+// function animate(){
+// 	$('.animate').animate({
+// 		top: "-=1300"
+// 	}, 1000, function() {
+// 		//
+// 	});
+// }
+
+// $('.result img').click(function() {
+// 	$('.result img').removeClass('animate').css('top','-260px');
+// 	$(this).addClass('animate');
+// 	animate();
+// });
+
+(function(){
+ 
+	console.log('Ran');
+	var animate = true;
+	console.log("animate set:", animate);
+
+	$('.result img').click(function() {
+		if(animate === true){
+			animate = false
+			$(this).animate({
+			    left: "-=1300"
+			  }, 1000, function() {
+			    animate
+		 	});
+		}
+	});
+
+})()
